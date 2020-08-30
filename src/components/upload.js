7b6d2c5940650baa38b7b6985c3aac958eac3d8e
cojenco <icathyouyang@gmail.com>
2020-08-30 16:31:30 -0700

    add FileReader to Upload callback. testng file readAsDataURL and readAsArrayBuffer

--- src/components/upload.js
+++ src/components/upload.js
@@ -3,7 +3,16 @@ import {useDropzone} from 'react-dropzone';
 
 
 function Upload (props) {
-  const {acceptedFiles, getRootProps, getInputProps} = useDropzone();
+  const {acceptedFiles, getRootProps, getInputProps} = useDropzone({
+    onDropHandler(files) {      
+      var file = files[0]
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        console.log(event.target.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  });
 
   const files = acceptedFiles.map(file => (
     <li key={file.path}>
@@ -19,6 +28,18 @@ function Upload (props) {
 
       acceptedFiles.map(file => {
         console.log(file)
+        
+
+        const reader = new FileReader();
+        reader.onload = (event) => {
+          console.log(event.target.result);
+        };
+        const data = reader.readAsDataURL(file);
+        // const data = reader.readAsArrayBuffer(file);
+        console.log(data);
+        // https://developer.mozilla.org/en-US/docs/Web/API/FileReader/readAsDataURL
+        // The blob's result cannot be directly decoded as Base64 without first removing the Data-URL declaration preceding the Base64-encoded data. To retrieve only the Base64 encoded string, first remove data:*/*;base64, from the result.
+        // call cloud function 
       })
       // do something: make an http call to the uploadfile function I wrote
     }
