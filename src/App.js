bebdeb81c76c3ac8c7d001476a5baa243e7f726b
cojenco <icathyouyang@gmail.com>
2020-08-29 22:15:24 -0700

    add FirebaseUI for Google OAuth

--- src/App.js
+++ src/App.js
@@ -1,15 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 import Upload from './components/upload'
+var firebase = require('firebase');
+var firebaseui = require('firebaseui');
 
 function App() {
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCPUxXrRR6ZdlRK1GHIrboFFWVu3VVOUwA",
+    authDomain: "keen-boulder-286521.firebaseapp.com",
+    databaseURL: "https://keen-boulder-286521.firebaseio.com",
+    projectId: "keen-boulder-286521",
+    storageBucket: "keen-boulder-286521.appspot.com",
+    messagingSenderId: "745284270797",
+    appId: "1:745284270797:web:fefc690c0470a662550784",
+    measurementId: "G-ZVCN44M5GN"
+  };
 
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  // Initialize the FirebaseUI Widget using Firebase.
+  var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+  var uiConfig = {
+    callbacks: {
+      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+        // User successfully signed in.
+        // Return type determines whether we continue the redirect automatically
+        // or whether we leave that to developer to handle.
+        console.log(authResult)
+        // console.log(authResult.credential.idToken)
+        console.log(authResult.additionalUserInfo.profile.id)
+        return true;
+      },
+      uiShown: function() {
+        // The widget is rendered.
+        // Hide the loader.
+        document.getElementById('loader').style.display = 'none';
+      }
+    },
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    signInFlow: 'popup',
+    signInSuccessUrl: '#',  //'<url-to-redirect-to-on-success>'
+    signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID
+    ],
+    // tosUrl: '<your-tos-url>',  // Terms of service url.
+    // privacyPolicyUrl: '<your-privacy-policy-url>'
+  };
+
+  // The start method will wait until the DOM is loaded.
+  ui.start('#firebaseui-auth-container', uiConfig);
   
   return (
     <div className="App">
 
       <h1>Eat a Rainbow!</h1>
+      <div id="firebaseui-auth-container"></div>
+      <div id="loader">Loading...</div>
+
       <Upload />
     </div>
   );
--- src/components/upload.js
+++ src/components/upload.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import {useDropzone} from 'react-dropzone';
-const fs = require('fs');
-const { Storage } = require('@google-cloud/storage');
-const storage = new Storage();
+
 
 function Upload (props) {
   const {acceptedFiles, getRootProps, getInputProps} = useDropzone();
 
-  
   const files = acceptedFiles.map(file => (
     <li key={file.path}>
       {file.path} - {file.size} bytes
@@ -16,18 +13,23 @@ function Upload (props) {
 
   const onUploadClick = () => {
     console.log('preparing to upload')
+
     if (acceptedFiles.length) {
       console.log('detect files')
+
+      acceptedFiles.map(file => {
+        console.log(file)
+      })
       // do something: make an http call to the uploadfile function I wrote
     }
+
   }
   
-
   return (
     <section className="container">
       <div {...getRootProps({className: 'dropzone'})}>
         <input {...getInputProps()} />
-        <p>Drag 'n' drop some files here, or click to select files</p>
+        <p>Drag and drop a file here, or click to select files</p>
       </div>
       <button onClick={onUploadClick} > UPLOAD </button>
       <aside>
@@ -39,3 +41,16 @@ function Upload (props) {
 }
 
 export default Upload;
+
+
+// File {path: "chairs.png", name: "chairs.png", lastModified: 1592551363428, 
+// lastModifiedDate: Fri Jun 19 2020 00:22:43 GMT-0700 (Pacific Daylight Time), 
+// webkitRelativePath: "", …}
+// lastModified: 1592551363428
+// lastModifiedDate: Fri Jun 19 2020 00:22:43 GMT-0700 (Pacific Daylight Time) {}
+// name: "chairs.png"
+// path: "chairs.png"
+// size: 927462
+// type: "image/png"
+// webkitRelativePath: ""
+// __proto__: File
