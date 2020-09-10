26dc48f35fecbc9455e4648e6fe95a9bbde4ef3b
cojenco <icathyouyang@gmail.com>
2020-09-09 19:17:15 -0700

    revise and remove comments

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Upload2 from './components/upload2'
 import Summary from './components/summary'
 import rainbowImage from './rainbowFood.jpg'
 var firebase = require('firebase');
-var firebaseui = require('firebaseui');
 
 function App() {
   // Your web app's Firebase configuration
@@ -25,7 +24,7 @@ function App() {
     firebase.analytics();
   }
 
-  // uID state to store uID
+  // store uID and username
   const [uID, setuID] = useState('')
   const [username, setUsername] = useState('')
 
@@ -33,18 +32,13 @@ function App() {
   firebase.auth().signInWithPopup(provider).then(function(result) {
     // This gives you a Google Access Token. You can use it to access the Google API.
     var token = result.credential.accessToken;
-    console.log(token);
     // The signed-in user info.
     var user = result.user;
-    console.log(user);
-    console.log(user.l);
-    console.log(result.additionalUserInfo.profile)
+    // console.log(user);
+    // console.log(result.additionalUserInfo.profile)
     console.log(result.additionalUserInfo.profile.id)
     setuID(result.additionalUserInfo.profile.id)
     setUsername(result.additionalUserInfo.profile.given_name)
-    // do something else
-    // call a cloud function to save new user document
-    // use uID stored in state to upload image
   }).catch(function(error) {
     var errorCode = error.code;
     var errorMessage = error.message;
@@ -79,42 +73,3 @@ function App() {
 }
 
 export default App;
-
-
-
-  ////// Initialize the FirebaseUI Widget using Firebase. //////
-  // var ui = new firebaseui.auth.AuthUI(firebase.auth());
-  // var uiConfig = {
-  //   callbacks: {
-  //     signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-  //       // User successfully signed in.
-  //       // Return type determines whether we continue the redirect automatically
-  //       // or whether we leave that to developer to handle.
-  //       console.log(authResult)
-  //       // console.log(authResult.credential.idToken)
-  //       console.log(authResult.additionalUserInfo.profile.id)
-  //       return true;
-  //     },
-  //     uiShown: function() {
-  //       // The widget is rendered.
-  //       // Hide the loader.
-  //       document.getElementById('loader').style.display = 'none';
-  //     }
-  //   },
-  //   // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-  //   signInFlow: 'popup',
-  //   signInSuccessUrl: '#',  //'<url-to-redirect-to-on-success>'
-  //   signInOptions: [
-  //     // Leave the lines as is for the providers you want to offer your users.
-  //     firebase.auth.GoogleAuthProvider.PROVIDER_ID
-  //   ],
-  //   // tosUrl: '<your-tos-url>',  // Terms of service url.
-  //   // privacyPolicyUrl: '<your-privacy-policy-url>'
-  // };
-
-  // // The start method will wait until the DOM is loaded.
-  // ui.start('#firebaseui-auth-container', uiConfig);
-  ////// Initialize the FirebaseUI Widget using Firebase. //////
-
-  // <div id="firebaseui-auth-container"></div>
-  // <div id="loader">Loading...</div> 
