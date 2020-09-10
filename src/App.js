2c0ecdb9ef434b8d3708c01f36b841444cceb583
cojenco <icathyouyang@gmail.com>
2020-09-09 18:10:14 -0700

    revise image banner

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import './App.css';
 import Upload2 from './components/upload2'
 import Summary from './components/summary'
+import rainbowImage from './rainbowFood.jpg'
 var firebase = require('firebase');
 var firebaseui = require('firebaseui');
 
@@ -55,8 +56,8 @@ function App() {
   return (
     <div className="App">
       <img 
-        // src="rainbow-food.jpg" 
-        src="https://lh3.googleusercontent.com/lOO6fNTGSvuaxzss_51bEQH9lyiHLYTFT0MtaiZeGpPL-dcWrV--0grms_BRjDcpgi9YHqh3F8jRSxn-s7SusvtHKqa1G7I4UbaDrjCFz5s9gDsuu9_3W-gicCICOYB-bBSoXReSMShyFMpdQLyEe5XZFkNOpQaHrEuY51jW7eJYLPFOb2OnIJgvN1DVekKDAm8qjj5SyDaiDVrXTMuCVpWUVUvqcL10FqTzcLm5YSip2GEHttSAZAgrkTpzRC9fmtyo5AfdAdBdqPSzGX30ZlyReutTCx2a7880yp3dDXo-kdPLG2hncJ4FsuoegMWXXVk5u3zVgFV6GSl_nt-OhgsKkRexENHBqTreRQ72k_PQ2dpzAW5_0geOoSlcUqMY3o2Nor84qwyo3jvOegL8MSLJ81xGDMUy33_z7uCbXCxro_dPDT9VQnG7AbbGbA_OObaW3BjAQpfKYBtzh2WD3ZPlHCF-sneei37sOk6pce112WqHL6QOCjHwjAyoLII6fG48uZUn45VCiw8TID0H333DpVdMKPfi0sWW0yPbVtzakAS51YtH0_uw9l_hZG19wvZMtA_uXN8gu45KJvghoOZZoyXMQIPNPb5eaOLtDmiS4Qulc46d1haf5cOAq9VnHzS9pvPAx2w1odz-QjQ2MZ5K95xSPF_UGjUjCTJnB2Dh8FWa-M5ztE60GpBxKw=w780-h521-no?authuser=0" 
+        src={rainbowImage} 
+        // src="https://lh3.googleusercontent.com/lOO6fNTGSvuaxzss_51bEQH9lyiHLYTFT0MtaiZeGpPL-dcWrV--0grms_BRjDcpgi9YHqh3F8jRSxn-s7SusvtHKqa1G7I4UbaDrjCFz5s9gDsuu9_3W-gicCICOYB-bBSoXReSMShyFMpdQLyEe5XZFkNOpQaHrEuY51jW7eJYLPFOb2OnIJgvN1DVekKDAm8qjj5SyDaiDVrXTMuCVpWUVUvqcL10FqTzcLm5YSip2GEHttSAZAgrkTpzRC9fmtyo5AfdAdBdqPSzGX30ZlyReutTCx2a7880yp3dDXo-kdPLG2hncJ4FsuoegMWXXVk5u3zVgFV6GSl_nt-OhgsKkRexENHBqTreRQ72k_PQ2dpzAW5_0geOoSlcUqMY3o2Nor84qwyo3jvOegL8MSLJ81xGDMUy33_z7uCbXCxro_dPDT9VQnG7AbbGbA_OObaW3BjAQpfKYBtzh2WD3ZPlHCF-sneei37sOk6pce112WqHL6QOCjHwjAyoLII6fG48uZUn45VCiw8TID0H333DpVdMKPfi0sWW0yPbVtzakAS51YtH0_uw9l_hZG19wvZMtA_uXN8gu45KJvghoOZZoyXMQIPNPb5eaOLtDmiS4Qulc46d1haf5cOAq9VnHzS9pvPAx2w1odz-QjQ2MZ5K95xSPF_UGjUjCTJnB2Dh8FWa-M5ztE60GpBxKw=w780-h521-no?authuser=0" 
         alt="a rainbow of veggies and fruit authorized by Happy Healthy Couple"
         className="w-50">
       </img>
@@ -65,13 +66,13 @@ function App() {
       <p className="blue-font">Hi {username}! </p>
 
       <div className="d-flex flex-column justify-content-center flex-wrap my-5">
-        <div className="h-25 add-topic"> <h3> Add <br></br> color </h3> </div>
-        <div className="h-25"><Upload2 uID={uID} /></div>
+        <div className="h-25 add-topic my-2"> <h3> Add <br></br> color </h3> </div>
+        <div className="h-25 my-2"><Upload2 uID={uID} /></div>
       </div>
 
       <div className="d-flex flex-column justify-content-center flex-wrap my-5">
-        <div className="h-25"> <h3> Get <br></br> summary </h3> </div>
-        <div className="h-75"> <span className="summary-line">Have you been eating colorfully in the past 7 days? </span><Summary uID={uID} /> </div>
+        <div className="h-25 my-2"> <h3> Get <br></br> summary </h3> </div>
+        <div className="h-75 my-2"> <p className="summary-line">Have you been eating colorfully in the past 7 days? </p> <Summary uID={uID} /> </div>
       </div>
     </div>
   );
