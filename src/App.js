b77cf3bddd580301cba3ce2e50f2af78ff45450f
cojenco <icathyouyang@gmail.com>
2020-09-14 18:16:20 -0700

    config dot env

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ var firebase = require('firebase');
 function App() {
   // Your web app's Firebase configuration
   var firebaseConfig = {
-    apiKey: "AIzaSyCPUxXrRR6ZdlRK1GHIrboFFWVu3VVOUwA",
-    authDomain: "keen-boulder-286521.firebaseapp.com",
-    databaseURL: "https://keen-boulder-286521.firebaseio.com",
-    projectId: "keen-boulder-286521",
-    storageBucket: "keen-boulder-286521.appspot.com",
-    messagingSenderId: "745284270797",
-    appId: "1:745284270797:web:fefc690c0470a662550784",
-    measurementId: "G-ZVCN44M5GN"
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASEURL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APPID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
   };
 
   // Initialize Firebase and avoid duplicates
